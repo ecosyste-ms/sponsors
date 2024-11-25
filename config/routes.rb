cb3a842369dce67c4ec1bfb458fd96502ab6913a
Andrew Nesbitt <andrewnez@gmail.com>
2024-11-25 18:36:46 +0000

    Refactor Dockerfile for Alpine-based image and update dependencies; enhance README and add development documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
+require 'sidekiq_unique_jobs/web'
+require 'sidekiq/web'
+
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+end if Rails.env.production?
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq"
+  mount PgHero::Engine, at: "pghero"
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :accounts, only: [:index, :show]
