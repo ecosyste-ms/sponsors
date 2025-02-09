0e4436ebc30901e4bd14cb1f2243300fda098974
Andrew Nesbitt <andrewnez@gmail.com>
2025-02-09 16:26:51 +0000

    Add custom headers to application responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  before_action :set_custom_headers
+
+  private
+
+  def set_custom_headers
+    response.set_header('Organization', 'Ecosyste.ms (contact@ecosyste.ms)')
+
+    response.set_header('Link', [
+      '<https://creativecommons.org/licenses/by-sa/4.0/>; rel="license"',
+      '<https://github.com/ecosyste-ms>; rel="source"',
+      '<https://opencollective.com/ecosystems>; rel="payment"',
+      '<mailto:contact@ecosyste.ms>; rel="author"',
+      '<https://ecosyste.ms/privacy>; rel="privacy-policy"',
+      '<https://ecosyste.ms/terms>; rel="terms-of-service"',
+      '<https://ecosyste.ms>; rel="self"',
+      '<https://ecosyste.ms>; rel="home"',
+      '<https://ecosyste.ms/about>; rel="about"',
+      '<https://ecosyste.ms>; rel="cite-as"',
+      '<https://mastodon.social/@ecosystems>; rel="me"',
+    ].join(", "))
+  end
 end
