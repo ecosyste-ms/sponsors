eb7969ec47fb71f9d74ef1336088b2eb34a1c036
Andrew Nesbitt <andrewnez@gmail.com>
2025-02-21 16:49:10 +0000

    Refactor styles and update meta description for application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,15 @@ module ApplicationHelper
   end
 
   def meta_description
-    @meta_description || 'An open API service aggregating public data about GitHub Sponsors.'
+    @meta_description || app_description
+  end
+
+  def app_name
+    "Sponsors"
+  end
+
+  def app_description
+    'An open API service aggregating public data about GitHub Sponsors.'
   end
 
   def sanitize_user_url(url)
